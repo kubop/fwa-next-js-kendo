1b4d492bf02a0d53f7c801cbade6615e70070a35
Jakub Pernica <jakub.pernica@apsoft.sk>
2023-05-02 15:57:08 +0200

    Kendo grid

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,6 @@
 import '@/styles/globals.css'
+import '@progress/kendo-theme-default/dist/all.css'
+
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
